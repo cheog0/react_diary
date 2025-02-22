edca745d0d02baa3708aafdd7856dad59c48be3a
cheog0 <clf0914@naver.com>
2025-02-22 21:42:47 +0900

    UI_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,78 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import Button from "./components/Button";
-import Header from "./components/Header";
-function App() {
-  const nav = useNavigate();
+import { useReducer, useRef, createContext } from "react";
 
-  const onClickButton = () => {
-    nav("/new");
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data));
+    default:
+      return state;
+  }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
   };
+
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: id,
+    });
+  };
+
   return (
-    <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={123}
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-      <Button
-        text={123}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-      <Button
-        text={123}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 클릭");
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onDelete, onUpdate }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary/:id" element={<Diary />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,4 +1,16 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editer from "../components/Editor";
+
 const New = () => {
-  return <div>new</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로가기"} />}
+      />
+      <Editer />
+    </div>
+  );
 };
 export default New;
